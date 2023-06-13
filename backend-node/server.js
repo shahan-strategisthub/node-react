f88d235263942826a03c7f941ce4cdaf1fec1bdb
shahan-shah <128580481+shahan-strategisthub@users.noreply.github.com>
2023-06-13 15:49:09 +0500

    update

--- backend-node/server.js
+++ backend-node/server.js
@@ -25,19 +25,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/all/users", (req, res) => {
   const getUsers = "SELECT * FROM users";
-  con.query(getUsers, (err, result) => {
-    res.json(result);
-  });
+
+  try {
+    con.query(getUsers, (err, result) => {
+      res.json(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.get("/api/users/:id", (req, res) => {
   const { id } = req.params;
-  const showUser = "SELECT * FROM  users WHERE id =?";
-  con.query(showUser, id, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-  });
+
+  const showUser = `SELECT * FROM  users WHERE id = ${req.params.id}`;
+
+  try {
+    con.query(showUser, id, (err, result) => {
+      if (result === null) {
+        res.status(404).send("Message : User not found");
+      } else {
+        res.status(200).json(result);
+      }
+
+      if (err) {
+        console.log(err);
+      }
+      console.log(result);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.post("/api/users", (req, res) => {
@@ -50,9 +68,13 @@ app.post("/api/users", (req, res) => {
     req.body.cell_phone,
     req.body.age,
   ];
-  con.query(createUser, [values], (err, result) => {
-   return result;
-  });
+
+  try {
+    con.query(createUser, [values], (err, result) => {});
+    res.status(201).send("Status: OK");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.put("/api/users/:id", (req, res) => {
@@ -60,36 +82,39 @@ app.put("/api/users/:id", (req, res) => {
   const { first_name, last_name, email, cell_phone, age } = req.body;
   const updateUser =
     "UPDATE  users SET first_name= ?,last_name= ?,email= ?,cell_phone= ?,age= ? WHERE id = ?";
-  con.query(
-    updateUser,
-    [first_name, last_name, email, cell_phone, age, id],
-    (err, result) => {
-      if (err) {
-        console.log(err);
+
+  try {
+    con.query(
+      updateUser,
+      [first_name, last_name, email, cell_phone, age, id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        }
       }
-    }
-  );
+    );
+    res.status(200).send("Status: OK");
+  } catch (error) {
+    console.log(error);
+  }
 });
-
-
 
 app.delete("/api/users/:id", (req, res) => {
   const { id } = req.params;
   const removeUser = "DELETE FROM  users WHERE id =?";
-  con.query(removeUser, id, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-  });
+
+  try {
+    con.query(removeUser, id, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+    res.status(200).send("Status: OK");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.get("/", (req, res) => {
-  const sqlInsert =
-    "INSERT INTO users (first_name,last_name,email,cell_phone,age) VALUES ('john','wick','johnwick@gmail.com','03034477063','66')";
-  con.query(sqlInsert, (err, result) => {
-    console.log(result);
-  });
-});
 app.listen(5000, () => {
   console.log("Server Started on port 5000");
 });
