dd30e9b84a2eb2b42925b094e287f3f5e28ad87f
shahan-shah <128580481+shahan-strategisthub@users.noreply.github.com>
2023-06-13 12:27:16 +0500

    Update server.js

--- backend-node/server.js
+++ backend-node/server.js
@@ -23,8 +23,63 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
+app.get("/api/all/users", (req, res)=>{
+    const getUsers = "SELECT * FROM users";
+    con.query(getUsers, (err,result)=>{
+       res.json(result);
+    })
+} )
 
-app.get("/api", (req , res) =>{
-    res.json({"users":["user1", "user2", "user2"]})
+app.get("/api/users/:id", (req, res)=>{
+    const {id}= req.params;
+    const showUser =
+        "SELECT * FROM  users WHERE id =?";
+    con.query(showUser,id, (err,result)=>{
+        if (err){
+            console.log(err);
+        }
+    });
+})
+
+app.post("/api/users", (req, res)=>{
+    const {first_name ,last_name, email, cell_phone , age  }= req.body;
+    const createUser  ="INSERT INTO users (first_name,last_name,email,cell_phone,age) VALUES (?,?,?,?,?)";
+    con.query(createUser,[first_name,last_name,email,cell_phone,age], (err,result)=>{
+        if (err){
+            console.log(err);
+        }
+    });
+})
+
+app.put("/api/users/:id", (req, res)=>{
+    const {id}= req.params;
+    const {first_name ,last_name, email, cell_phone , age  }= req.body;
+    const updateUser =
+        "UPDATE  users SET first_name= ?,last_name= ?,email= ?,cell_phone= ?,age= ? WHERE id = ?";
+    con.query(updateUser,[first_name,last_name,email,cell_phone,age,id], (err,result)=>{
+        if (err){
+            console.log(err);
+        }
+    });
+})
+
+
+app.delete("/api/users/:id", (req, res)=>{
+    const {id}= req.params;
+    const removeUser =
+        "DELETE FROM  users WHERE id =?";
+    con.query(removeUser,id, (err,result)=>{
+        if (err){
+            console.log(err);
+        }
+    });
+})
+
+app.get("/", (req , res) =>{
+    const sqlInsert ="INSERT INTO users (first_name,last_name,email,cell_phone,age) VALUES ('john','wick','johnwick@gmail.com','03034477063','66')";
+    con.query(sqlInsert,(err,result)=>{
+        console.log(result);
+    })
+
 })
 app.listen(5000, () => {console.log("Server Started on port 5000")})
